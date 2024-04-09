090ae749a5e5a4d5a71342b7122c855618dde441
Ruben Rosenmeyer <ruben.rosenmeyer@gmail.com>
2024-04-09 15:18:43 +0200

    added reservation form on uebernachtung.html & .css

--- project/scripts.js
+++ project/scripts.js
@@ -1,3 +1,7 @@
+var nextYear= new Date();
+nextYear.setFullYear(new Date().getFullYear() + 1);
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("backButton").addEventListener("click", () => { history.back(); });
+    document.getElementById("calendar").setAttribute("min", new Date().toISOString().split("T")[0]);
+    document.getElementById("calendar").setAttribute("max", nextYear.toISOString().split("T")[0]);
 });
