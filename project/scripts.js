d52f623ce43be9ea1dd39675285666dcd2678880
Ruben Rosenmeyer <ruben.rosenmeyer@gmail.com>
2024-04-11 17:39:27 +0200

    added featherlight functionality

--- project/scripts.js
+++ project/scripts.js
@@ -5,3 +5,22 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("calendar").setAttribute("min", new Date().toISOString().split("T")[0]);
     document.getElementById("calendar").setAttribute("max", nextYear.toISOString().split("T")[0]);
 });
+
+$(document).ready(function(){
+	$('.gallery').featherlightGallery();
+    $.featherlightGallery.prototype.afterContent = function() {
+        var caption = this.$currentTarget.find('img').attr('alt');
+        this.$instance.find('.caption').remove();
+        $('<div class="caption">').text(caption).appendTo(this.$instance.find('.featherlight-content'));
+        }; 
+    /* Alternative (format: <a href="...alt="text">). This will keep the last tag for all following images, if there is no new one
+    (so you'll need to give every image an alt):
+        $('.labeled_gallery').featherlightGallery({
+        filter: "a",
+        afterContent: function() {
+            this.$legend = this.$legend || $('<div class="legend"/>').insertAfter(this.$content);
+            this.$legend.text(this.$currentTarget.attr('alt'));
+        }
+    });
+    */
+});
